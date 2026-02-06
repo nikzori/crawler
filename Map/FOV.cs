c11f82d64be208416295efb94f90b14b4627d50e
nikzori <kzr.niki@gmail.com>
2026-02-06 16:21:57 +0300

    shadowcasting done

--- Map/FOV.cs
+++ Map/FOV.cs
@@ -4,6 +4,13 @@
 
 public static class FOV
 {
+    public static Transform[] transforms =
+    {
+        new Transform(1, 0, 0, 1), // NW-NE
+        new Transform(0, 1, 1, 0), // NE-SE
+        new Transform(-1, 0, 0, -1), // SE-SW
+        new Transform(0, -1, -1, 0) // SW-NW
+    };
     /// <summary>
     /// Renderer-agnostic shadowcast in a square around target (player) position.
     /// </summary>
@@ -22,66 +29,77 @@ public static class FOV
 
         result[viewerPos.X, viewerPos.Y] = true; //set player tile as visible
 
-        // slopes are defined with two points as (x1 - x2) / (y1 - y2), where [x1,y1] is a point on the map and [x2,y2] is the player pos
-        // we use slopes to defy an quadrand (?), e.g. slopes of -1 and 1 defy area from NW diagonal to NE diagonal
+        // slopes are defined with two points as (x1 - x2) / (y1 - y2), where [x1,y1] is a point on the map and [x2,y2] is the viewer(player) pos
+        // since we're using local coordinates with viewer at [0,0], slopes become (x/y). 
+        // Then we add local [x,y] to global coords, multiplying x and y to "rotate" coordinates (see Transform[] transforms above)
+        //
+        // We use slopes to defy an quadrand (?), e.g. slopes of -1 and 1 defy area from NW diagonal to NE diagonal
         // for each quadrand, we go row by row for vertical ones and column by column for horizontal ones
         // if the slope between a point on the map and playerPos is in the range of quadrand, that point is visible
         // if we run into an obstacle, we use it's position to call the same method using the obstacle's slope as one of the limiting slopes
         // we continue the initial method call, looking for an open cell. If we find one, we use its pos to assign a new left slope for this call
-        // if we end the row on an obstacle, we break the loop.
+        // if we end a row on an obstacle, we break the loop.
         // that way, we call separate methods for each gap made by obstacles recursively
 
-        LumosMaxima(map, viewerPos, viewerPos.Y + 1, visionRange, -1f, 1f, ref result);
+        for (int i = 0; i < transforms.Length; i++)
+            LumosMaxima(map, viewerPos, 1, visionRange, -1f, 1f, transforms[i], ref result);
 
         return result;
     }
 
     private static void LumosMaxima(Map map, Vector2Int viewerPos, int startColumn, int visionRange,
-            float leftSlope, float rightSlope, ref bool[,] result)
+            float leftSlope, float rightSlope, Transform transform, ref bool[,] result)
     {
         try
         {
             bool prevWasBlocked = false;
-            bool newLeftSlopeNeeded = false;
-            for (int yc = startColumn; yc <= viewerPos.Y + visionRange; yc++) // outer loop with y coords
+
+            for (int yc = startColumn; yc <= visionRange; yc++) // outer loop with y coords
             {
                 if (prevWasBlocked)
                     break;
 
-                float newLeftSlope = leftSlope;
-                float newRightSlope = rightSlope; //just in case
-                for (int xc = viewerPos.X - visionRange; xc <= viewerPos.X + visionRange; xc++)
+                float slopeBuffer = -1;
+                float newRightSlope = rightSlope;
+                for (int xc = -visionRange; xc <= visionRange; xc++)
                 {
-                    if (xc < 1 || xc >= result.GetLength(0) - 1 || yc < 1 || yc >= result.GetLength(1) - 1) // OOB check
+                    int realX = viewerPos.X + xc * transform.xx + yc * transform.xy;
+                    int realY = viewerPos.Y + xc * transform.yx + yc * transform.yy;
+
+                    if (realX < 1 || realX >= result.GetLength(0) - 1 || realY < 1 || realY >= result.GetLength(1) - 1) // OOB check
                         continue;
 
-                    float posSlopeLeft = (xc - viewerPos.X - 0.5f) / (yc - viewerPos.Y + 0.5f);
-                    float posSlopeRight = (xc - viewerPos.X + 0.5f) / (yc - viewerPos.Y + 0.5f);
+                    float posSlopeLeft = (xc - 0.5f) / (yc + 0.5f);
+                    if (posSlopeLeft < leftSlope)
+                        continue;
+                    float posSlopeRight = (xc + 0.5f) / (yc + 0.5f);
+                    if (posSlopeRight > rightSlope)
+                        break;
 
-                    if (posSlopeLeft >= leftSlope && posSlopeRight <= rightSlope)
+                    Vector2Int currentPos = new(realX, realY);
+                    result[realX, realY] = true; // everything between slopes is visible 
+                    Cell cell = map.cells[currentPos];
+                    cell.SetRevealed(true);
+
+                    if (prevWasBlocked)
                     {
-                        Vector2Int currentPos = new(xc, yc);
-                        result[xc, yc] = true; // because we're limited by the slopes, every cell between them will be visible
-                        Cell cell = map.cells[currentPos];
-                        cell.SetRevealed(true);
                         if (!cell.IsTransparent)
-                        {
-                            prevWasBlocked = true;
-                            newRightSlope = posSlopeLeft;
-                            if (!newLeftSlopeNeeded)
-                            {
-                                newLeftSlopeNeeded = true;
-                                LumosMaxima(map, viewerPos, yc + 1, visionRange, leftSlope, newRightSlope, ref result);
-                            }
-                        }
+                            slopeBuffer = posSlopeRight;
                         else
                         {
                             prevWasBlocked = false;
-                            if (newLeftSlopeNeeded)
-                            {
-                                leftSlope = posSlopeLeft;
-                                newLeftSlopeNeeded = false;
-                            }
+                            leftSlope = slopeBuffer;
+                        }
+                    }
+                    else
+                    {
+                        if (!cell.IsTransparent)
+                        {
+                            if (posSlopeLeft >= leftSlope)
+                                LumosMaxima(map, viewerPos, yc + 1, visionRange, leftSlope, posSlopeLeft, transform, ref result);
+
+                            prevWasBlocked = true;
+                            slopeBuffer = posSlopeRight;
                         }
                     }
                 }
@@ -90,9 +108,7 @@ public static class FOV
         }
         catch (Exception e)
         {
-            Game.Log(e.Message);
-            // string LogFilePath = AppContext.BaseDirectory + "/ShadowcastErrorLog.txt";
-            // File.AppendAllText(LogFilePath, e.Message);
+            File.WriteAllText(AppContext.BaseDirectory + "/FOVLog.txt", e.Message);
         }
 
     }
@@ -103,13 +119,25 @@ public static class FOV
     {
         return false;
     }
-
 }
-
-public enum QuadrantDirection { North, East, South, West }
 
 public struct Slope
 {
     public readonly int X, Y;
     public Slope(int x, int y) { X = x; Y = y; }
+}
+
+public class Transform
+{
+    public int xx { get; private set; }
+    public int xy { get; private set; }
+    public int yx { get; private set; }
+    public int yy { get; private set; }
+    public Transform(int xx, int xy, int yx, int yy)
+    {
+        this.xx = xx;
+        this.xy = xy;
+        this.yx = yx;
+        this.yy = yy;
+    }
 }
